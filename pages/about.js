db1dba98e58f65bfe843c6b1a8f9fc7960e303e8
Filip Jerga <jerga99@gmail.com>
2019-09-13 12:59:11 +0200

    3 Class Components

--- pages/about.js
+++ pages/about.js
@@ -8,6 +8,17 @@ const About = () => {
     <h1>Hello About Page - {message}</h1>
   )
 }
+// WHEN TO USE:
+// 1. for smaller components
+// 2. reusable components
+// 3. presentional components , patialy right, we can use HOOKS and specify state
+
+// const About = () => {
+//   const message = 'Hello World'
+//   return (
+//     <h1>Hello About Page - {message}</h1>
+//   )
+// }
 
 // const About = () => {
 //   const message = 'Hello World'
@@ -15,11 +26,23 @@ const About = () => {
 // }
 
 // unctional component - normal function
+// functional component - normal function
 // function About() {
 //   return (
 //     <h1>Hello About Page</h1>
 //   )
 // }
+
+
+class About extends React.Component {
+  render() {
+    return (
+      <h1>Hello I am class component</h1>
+    )
+  }
+}
+
+
 
 
 export default About
