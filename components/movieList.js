8c9f008dc74fdfa004b7f9c7f9e5ac0421fd0d89
Filip Jerga <jerga99@gmail.com>
2019-09-16 11:35:01 +0200

    6 State Intro

--- components/movieList.js
+++ components/movieList.js
@@ -4,11 +4,38 @@ import React from 'react'
 
 class MovieList extends React.Component {
 
+  // state = {
+  //   count: 0
+  // }
 
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      count: 0
+    }
+  }
+
+  increment = () => {
+    alert('incrementing Number')
+  }
+
+  decrement = () => {
+    alert('decrementing Number')
+  }
 
   render() {
     return (
       <React.Fragment>
+
+        <div>
+          <button onClick={this.increment} className="btn btn-primary">Increment Number</button>
+          <button onClick={this.decrement} className="btn btn-primary">Decrement Number</button>
+
+          <h1>{this.state.count}</h1>
+        </div>
+
+
         <div className="col-lg-4 col-md-6 mb-4">
           <div className="card h-100">
             <a href="#"><img className="card-img-top" src="http://placehold.it/700x400" alt="" /></a>
