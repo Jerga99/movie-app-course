bc5f9fdf42870b08843f701a6e6e16fb91e06b01
Filip Jerga <jerga99@gmail.com>
2019-09-18 15:26:59 +0200

    14 Moves data and fetch function

--- components/movieList.js
+++ components/movieList.js
@@ -15,7 +15,7 @@ class MovieList extends React.Component {
   renderMovies(movies) {
     return movies.map(movie =>
       (
-        <div className="col-lg-4 col-md-6 mb-4">
+        <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
           <div className="card h-100">
             <a href="#"><img className="card-img-top" src={movie.image} alt="" /></a>
             <div className="card-body">
