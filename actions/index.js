9b9f504ed00d94f7d395b5fa1171702b47d5557b
Filip Jerga <jerga99@gmail.com>
2019-09-19 13:21:44 +0200

    24 Get Movie by ID

--- actions/index.js
+++ actions/index.js
@@ -40,6 +40,29 @@ export const getMovies = () => {
 }
 
 
+export const getMovieById = (id) => {
+
+  return new Promise((resolve, reject) => {
+    const movieIndex = MOVIE_DATA.findIndex(m => m.id === id)
+    const movie = MOVIE_DATA[movieIndex]
+
+    setTimeout(() => resolve(movie), 50)
+  })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,18 +1,42 @@
 
 import { useRouter } from 'next/router'
+import { getMovieById } from '../../actions'
 
 
-
-const Movie = () => {
+const Movie = (props) => {
   const router = useRouter()
   const { id } = router.query
+  const { movie } = props
 
   return (
     <div className="container">
-      <h1>Movie with id: { id } </h1>
+      <div className="jumbotron">
+        <h1 className="display-4">{ movie.name }</h1>
+        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <hr className="my-4" />
+        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+      </div>
+      <p>
+        Some description about the movie
+      </p>
     </div>
   )
 }
 
+Movie.getInitialProps = async () => {
+  const movie = await getMovieById("2")
+
+  return { movie }
+}
+
 
 export default Movie
+
+
+
+
+
+
+
+
