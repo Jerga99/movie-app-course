22fe99b47ef73bcb72005421e5b681ad963dd25b
Filip Jerga <jerga99@gmail.com>
2019-09-19 13:37:03 +0200

    26 Finishes detail page

--- actions/index.js
+++ actions/index.js
@@ -5,6 +5,7 @@ const MOVIE_DATA = [
     name: 'The Shawshank Redemption',
     releaseYear: 1994,
     description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     rating: 4.8,
     genre: 'drama',
     image: 'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg'
@@ -14,6 +15,7 @@ const MOVIE_DATA = [
     name: 'The Dark Knight',
     releaseYear: 2008,
     description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     rating: 4.7,
     genre: 'action, crime, drama',
     image: 'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600'
@@ -23,6 +25,7 @@ const MOVIE_DATA = [
     name: 'Lord of the Rings',
     releaseYear: 2004,
     description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     rating: 4.9,
     genre: 'adventure, drama, fantasy',
     image: 'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600'
--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -12,20 +12,26 @@ const Movie = (props) => {
     <div className="container">
       <div className="jumbotron">
         <h1 className="display-4">{ movie.name }</h1>
-        <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <p className="lead">{ movie.description }</p>
         <hr className="my-4" />
-        <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+        <p>{ movie.genre }</p>
         <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
       </div>
-      <p>
-        Some description about the movie
+      <p className="desc-text">
+        { movie.longDesc }
       </p>
+      <style jsx>{`
+        .desc-text {
+          font-size: 21px;
+        }
+      `}
+      </style>
     </div>
   )
 }
 
-Movie.getInitialProps = async () => {
-  const movie = await getMovieById("2")
+Movie.getInitialProps = async ({ query }) => {
+  const movie = await getMovieById(query.id)
 
   return { movie }
 }
