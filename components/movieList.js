9b8a8fd103b07984e1bfbfcd1c23eda5393dcff9
Filip Jerga <jerga99@gmail.com>
2019-09-19 13:28:36 +0200

    25 Fetch link to detail pages

--- components/movieList.js
+++ components/movieList.js
@@ -18,12 +18,12 @@ class MovieList extends React.Component {
       (
         <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
           <div className="card h-100">
-            <Link href={`/movies/${movie.id}`}>
+            <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
               <a><img className="card-img-top" src={movie.image} alt="" /></a>
             </Link>
             <div className="card-body">
               <h4 className="card-title">
-                <Link href={`/movies/${movie.id}`}>
+                <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
                   <a>{movie.name}</a>
                 </Link>
               </h4>
