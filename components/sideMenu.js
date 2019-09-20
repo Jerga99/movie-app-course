1be2cf260962f99ca9b0d7edadd1bebe8a3a0292
Filip Jerga <jerga99@gmail.com>
2019-09-20 10:11:00 +0200

    30 Adds modal + close Modal

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,12 +1,13 @@
 
 import { useState } from 'react'
-
+import Modal from './modal'
 
 
 const SideMenu = (props) => {
   const { categories } = props
   return (
     <div>
+      <Modal />
       <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
         { categories.map(c =>
