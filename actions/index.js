a7065b9236ad81c695bc81aacc55daa9eee8b912
Filip Jerga <jerga99@gmail.com>
2019-09-21 12:55:00 +0200

    39 Get movies from server

--- actions/index.js
+++ actions/index.js
@@ -1,40 +1,9 @@
 
-const MOVIE_DATA = [
-  {
-    id: '1',
-    name: 'The Shawshank Redemption',
-    releaseYear: 1994,
-    description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    rating: 4.8,
-    genre: 'drama',
-    image: 'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg',
-    cover: 'https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1640&q=80'
-  },
-  {
-    id: '2',
-    name: 'The Dark Knight',
-    releaseYear: 2008,
-    description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    rating: 4.7,
-    genre: 'action, crime, drama',
-    image: 'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600',
-    cover: 'https://images.unsplash.com/photo-1497124401559-3e75ec2ed794?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80'
+import axios from 'axios'
 
-  },
-  {
-    id: '3',
-    name: 'Lord of the Rings',
-    releaseYear: 2004,
-    description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-    longDesc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    rating: 4.9,
-    genre: 'adventure, drama, fantasy',
-    image: 'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600',
-    cover: 'https://images.unsplash.com/photo-1462759353907-b2ea5ebd72e7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2389&q=80'
-  }
-]
+const BASE_URL = 'http://localhost:3000'
+
+const MOVIE_DATA = []
 
 const CATEGORY_DATA = [
   {id: 'c-1', name: 'drama'},
@@ -58,12 +27,7 @@ export const getCategories = () => {
 }
 
 export const getMovies = () => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(MOVIE_DATA)
-      // reject('Cannot fetch data!')
-    }, 50)
-  })
+  return axios.get(`${BASE_URL}/api/v1/movies`).then(res => res.data)
 }
 
 export const createMovie = (movie) => {
--- server/index.js
+++ server/index.js
@@ -9,13 +9,15 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const moviesData = require('./data.json')
+
 app.prepare().then(() => {
 
   const server = express();
   server.use(bodyParser.json())
 
   server.get('/api/v1/movies', (req, res) => {
-    return res.json({message: 'Hello World'})
+    return res.json(moviesData)
   })
 
   server.post('/api/v1/movies', (req, res) => {
