a0f8104fa1e85f51e499ab2e9df0ea230ad83b47
Filip Jerga <jerga99@gmail.com>
2019-09-21 12:12:32 +0200

    38 More about server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@
 
 const next = require('next')
 const express = require('express');
+const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
@@ -11,6 +12,38 @@ const handle = app.getRequestHandler()
 app.prepare().then(() => {
 
   const server = express();
+  server.use(bodyParser.json())
+
+  server.get('/api/v1/movies', (req, res) => {
+    return res.json({message: 'Hello World'})
+  })
+
+  server.post('/api/v1/movies', (req, res) => {
+    const movie = req.body
+    console.log(JSON.stringify(movie))
+    return res.json({...movie, createdTime: 'today', author: 'Filip'})
+  })
+
+  server.patch('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+    return res.json({message: `Updating post of id: ${id}`})
+  })
+
+  server.delete('/api/v1/movies/:id', (req, res) => {
+    const { id } = req.params
+    return res.json({message: `Deleting post of id: ${id}`})
+  })
+
+  // server.get('/faq', (req, res) => {
+  //   res.send(`
+  //     <html>
+  //       <head></head>
+  //       <body><h1>Hello World!</h1>
+  //       </body>
+  //     </html>
+  //   `)
+  // })
+
   // we are handling all of the request comming to our server
   server.get('*', (req, res) => {
     // next.js is handling requests and providing pages where we are navigating to
